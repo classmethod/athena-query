dfbee4a00b3e4071f0a7f92ad8e290ef2418fcba
yamatatsu <the.kinnikuman@gmail.com>
2023-01-27 00:04:51 +0900

    use pnpm

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "index.ts",
