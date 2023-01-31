0db059a164b3338efece833b518b30e78e2a92be
yamatatsu <the.kinnikuman@gmail.com>
2023-01-31 22:18:00 +0900

    chore: add jsdoc

--- lib/athena-query.ts
+++ lib/athena-query.ts
@@ -2,8 +2,20 @@ import * as helpers from "./helper";
 import type { Athena } from "@aws-sdk/client-athena";
 
 type Options = {
-  db?: string;
+  /**
+   * The name of the workgroup in which the query is being started.
+   */
   workgroup?: string;
+
+  /**
+   * The name of the database used in the query execution.
+   * The database must exist in the catalog.
+   */
+  db?: string;
+
+  /**
+   * The name of the data catalog used in the query execution.
+   */
   catalog?: string;
 };
 
@@ -13,10 +25,29 @@ export class AthenaQuery {
     private readonly options?: Options
   ) {}
 
+  /**
+   * @see https://github.com/classmethod/athena-query#usage
+   *
+   * @param sql
+   * @param options
+   */
   async *query(
     sql: string,
     options?: {
+      /**
+       * A list of values for the parameters in a query.
+       * The values are applied sequentially to the parameters in the query in the order in which the parameters occur.
+       */
       executionParameters?: (string | number | BigInt)[];
+
+      /**
+       * The maximum number of results (rows) to return in this request.
+       *
+       * @deprecated We recommend you to use LIMIT clause in SQL.
+       * Because even if you set it, athena-query will continue to retrieve results unless you break your for-loop.
+       *
+       * @see https://docs.aws.amazon.com/athena/latest/ug/select.html#select-parameters
+       */
       maxResults?: number;
     }
   ): AsyncGenerator<helpers.AtheneRecordData, void, undefined> {
